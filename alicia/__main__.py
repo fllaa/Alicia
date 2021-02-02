eb6cba2ae6c545cf62e77057fc9f628ba028f929
AkaruiAikara <fallahandyprakasa26@gmail.com>
2021-02-02 19:16:55 +0700

    reposition import modules system

--- alicia/__main__.py
+++ alicia/__main__.py
@@ -21,45 +21,6 @@ GDPR = []
 
 loop = asyncio.get_event_loop()
 
-for module_name in ALL_MODULES:
-    imported_module = importlib.import_module("alicia.modules." + module_name)
-    if not hasattr(imported_module, "__mod_name__"):
-        imported_module.__mod_name__ = imported_module.__name__
-
-    if imported_module.__mod_name__.lower() not in IMPORTED:
-        IMPORTED[imported_module.__mod_name__.lower()] = imported_module
-    else:
-        raise Exception(
-            "Can't have two modules with the same name! Please change one")
-
-    if hasattr(imported_module, "__help__") and imported_module.__help__:
-        HELPABLE[imported_module.__mod_name__.lower()] = imported_module
-
-    # Chats to migrate on chat_migrated events
-    if hasattr(imported_module, "__migrate__"):
-        MIGRATEABLE.append(imported_module)
-
-    if hasattr(imported_module, "__stats__"):
-        STATS.append(imported_module)
-
-    if hasattr(imported_module, "__gdpr__"):
-        GDPR.append(imported_module)
-
-    if hasattr(imported_module, "__user_info__"):
-        USER_INFO.append(imported_module)
-
-    if hasattr(imported_module, "__import_data__"):
-        DATA_IMPORT.append(imported_module)
-
-    if hasattr(imported_module, "__export_data__"):
-        DATA_EXPORT.append(imported_module)
-
-    if hasattr(imported_module, "__chat_settings__"):
-        CHAT_SETTINGS[imported_module.__mod_name__.lower()] = imported_module
-
-    if hasattr(imported_module, "__user_settings__"):
-        USER_SETTINGS[imported_module.__mod_name__.lower()] = imported_module
-
 
 async def reinitial():
     await alia.start()
@@ -70,6 +31,46 @@ async def reinitial():
 async def start_bot():
     await reinitial()
     await alia.start()
+
+    for module_name in ALL_MODULES:
+        imported_module = importlib.import_module("alicia.modules." + module_name)
+        if not hasattr(imported_module, "__mod_name__"):
+            imported_module.__mod_name__ = imported_module.__name__
+
+        if imported_module.__mod_name__.lower() not in IMPORTED:
+            IMPORTED[imported_module.__mod_name__.lower()] = imported_module
+        else:
+            raise Exception(
+                "Can't have two modules with the same name! Please change one")
+
+        if hasattr(imported_module, "__help__") and imported_module.__help__:
+            HELPABLE[imported_module.__mod_name__.lower()] = imported_module
+
+        # Chats to migrate on chat_migrated events
+        if hasattr(imported_module, "__migrate__"):
+            MIGRATEABLE.append(imported_module)
+
+        if hasattr(imported_module, "__stats__"):
+            STATS.append(imported_module)
+
+        if hasattr(imported_module, "__gdpr__"):
+            GDPR.append(imported_module)
+
+        if hasattr(imported_module, "__user_info__"):
+            USER_INFO.append(imported_module)
+
+        if hasattr(imported_module, "__import_data__"):
+            DATA_IMPORT.append(imported_module)
+
+        if hasattr(imported_module, "__export_data__"):
+            DATA_EXPORT.append(imported_module)
+
+        if hasattr(imported_module, "__chat_settings__"):
+            CHAT_SETTINGS[imported_module.__mod_name__.lower()] = imported_module
+
+        if hasattr(imported_module, "__user_settings__"):
+            USER_SETTINGS[imported_module.__mod_name__.lower()] = imported_module
+
     LOGGER.info("Alicia goes brrr...")
     await idle()
 
